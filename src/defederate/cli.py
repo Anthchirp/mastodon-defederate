dbf93841e4983e0e28326538a83e4ea741d27a3f
Markus Gerstel <markus.gerstel@osirium.com>
2022-11-12 20:28:28 +0000

    Add a simple command line interface

--- src/defederate/cli.py
+++ src/defederate/cli.py
@@ -1,2 +1,42 @@
+import argparse
+import sys
+
+import defederate
+
+
 def main():
-    print("Hello world")
+    parser = argparse.ArgumentParser(
+        usage="defederate <command> [<args>]",
+        description="""Tools for Mastodon Instance Blocklist Management (v{version})
+The most commonly used commands are:
+   show     Show currently active instance blocks
+   block    Add an instance block
+Each command has its own set of parameters, and you can get more information
+by running defederate <command> --help
+""".format(
+            version=defederate.__version__
+        ),
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument("subcommand", help=argparse.SUPPRESS)
+    # parse_args defaults to [1:] for args, but need to
+    # exclude the rest of the args too, or validation will fail
+    parameters = sys.argv[1:2]
+    if not parameters:
+        parser.print_help()
+        sys.exit(0)
+    args = parser.parse_args(parameters)
+    subcommand = globals().get("cli_" + args.subcommand)
+    if subcommand:
+        return subcommand(sys.argv[2:])
+    parser.print_help()
+    print()
+    sys.exit(f"Unrecognized command: {args.subcommand}")
+
+
+def cli_show(args):
+    print("Show", args)
+
+
+def cli_block(args):
+    print("Block", args)
